e7e0d5383689d26aec5808d6e1b0962dccf15cc4
Samuel <samuellm@live.com>
2018-07-30 20:53:05 -0300

    Added Controller for char infos

--- TibiaTracker/src/core/GetInfos.java
+++ TibiaTracker/src/core/GetInfos.java
@@ -4,12 +4,15 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GetInfos {
 
-	public void getFullCharacter(String strCharName) {
+	public Map<String, String> getFullCharacterInfo(String strCharName) {
 		try {
 			URL url = new URL(
 					"https://secure.tibia.com/community/?subtopic=characters&name=" + strCharName.replace(" ", "+"));
@@ -19,27 +22,32 @@ public class GetInfos {
 				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 				conn.setRequestProperty("User-Agent",
 						"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+				conn.setConnectTimeout(1000);
 				responseCode = conn.getResponseCode();
 
-				// open the stream and put it into BufferedReader
-				BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "ISO-8859-1"));
+				if (responseCode == 200) {
+					// open the stream and put it into BufferedReader
+					BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "ISO-8859-1"));
 
-				String inputLine;
+					String inputLine;
 
-				while ((inputLine = br.readLine()) != null) {
-					content += inputLine;
+					while ((inputLine = br.readLine()) != null) {
+						content += inputLine;
+					}
+					br.close();
 				}
-				br.close();
-				if (responseCode != 200) {
-					Thread.sleep(500);
-				}
+
+				conn.disconnect();
+				conn = null;
 			}
 
 			if (content.contains("does not exist.")) {
 				// TODO
 				// Player does not exist
 			} else {
-				String strName = "", strLevel = "", strVocation = "", strWorld = "", strSex = "", strAchievementPoints = "", strGuildMembership = "", strLastLogin = "", strAccountStatus = "", strFormerNames = "", strFormerWorld = "", strMarriedTo = "", strResidence = "", strHouse = "";
+				String strName = "", strLevel = "", strVocation = "", strWorld = "", strSex = "",
+						strAchievementPoints = "", strGuildMembership = "", strLastLogin = "", strAccountStatus = "",
+						strFormerNames = "", strFormerWorld = "", strMarriedTo = "", strResidence = "", strHouse = "";
 
 				// NAME
 				String patternName = "<td[\\s]*width=20%>Name:</td><td>[\\s\\w'-]*";
@@ -76,7 +84,7 @@ public class GetInfos {
 				while (mWorld.find()) {
 					strWorld = mWorld.group(0).split("><td>")[1].trim();
 				}
-				
+
 				// SEX
 				String patternSex = "<td>Sex:</td><td>[\\s\\w]*";
 				Pattern rSex = Pattern.compile(patternSex);
@@ -85,7 +93,7 @@ public class GetInfos {
 				while (mSex.find()) {
 					strSex = mSex.group(0).split("><td>")[1].trim();
 				}
-				
+
 				// RESIDENCE
 				String patternResidence = "<td>Residence:</td><td>[\\s\\w]*";
 				Pattern rResidence = Pattern.compile(patternResidence);
@@ -94,7 +102,7 @@ public class GetInfos {
 				while (mResidence.find()) {
 					strResidence = mResidence.group(0).split("><td>")[1].trim();
 				}
-				
+
 				// ACHIEVEMENT POINTS
 				String patternAchievementPoints = "<nobr>Achievement Points:</nobr></td><td>[\\s\\d]*";
 				Pattern rAchievementPoints = Pattern.compile(patternAchievementPoints);
@@ -103,7 +111,7 @@ public class GetInfos {
 				while (mAchievementPoints.find()) {
 					strAchievementPoints = mAchievementPoints.group(0).split("><td>")[1].trim();
 				}
-				
+
 				// LAST LOGIN
 				String patternLastLogin = "<td>Last Login:</td><td>[\\s\\w&#;,:]*";
 				Pattern rLastLogin = Pattern.compile(patternLastLogin);
@@ -111,9 +119,9 @@ public class GetInfos {
 
 				while (mLastLogin.find()) {
 					String p2 = mLastLogin.group(0).split(">")[3].replace(">", "").replace("&#160;", " ").trim();
-					strLastLogin  = p2;
+					strLastLogin = p2;
 				}
-				
+
 				// ACCOUNT STATUS
 				String patternAccountStatus = "<td>Account&#160;Status:</td><td>[\\s\\w]*";
 				Pattern rAccountStatus = Pattern.compile(patternAccountStatus);
@@ -122,7 +130,7 @@ public class GetInfos {
 				while (mAccountStatus.find()) {
 					strAccountStatus = mAccountStatus.group(0).split("><td>")[1].trim();
 				}
-				
+
 				// HOUSE
 				String patternHouse = "<td>House:</td><td>[\\s]*[<][\\w\\s]*[=\"]*[\\w:/.?=&;+\"\\s]*[>][\\w\\s]*[</a>\\s]*[(\\w)]*";
 				Pattern rHouse = Pattern.compile(patternHouse);
@@ -131,16 +139,18 @@ public class GetInfos {
 				while (mHouse.find()) {
 					strHouse = mHouse.group(0).split("\" >")[1].replace("</a>", "");
 				}
-				
+
 				// GUILD MEMBERSHIP
 				String patternGuildMembership = "<td>Guild&#160;Membership:</td><td>[\\s\\w]*[<][\\w\\s]*[=\"]*[\\w:/.?=&;+\"\\s]*[>][\\w\\s]*[&#\\d;]*[\\w]*";
 				Pattern rGuildMembership = Pattern.compile(patternGuildMembership);
 				Matcher mGuildMembership = rGuildMembership.matcher(content);
 
 				while (mGuildMembership.find()) {
-					strGuildMembership = mGuildMembership.group(0).split("Guild&#160;Membership:</td><td>")[1].replace("</a></td", "").replaceAll("<a[\\s]*href[\\s]*=[\\s]*\"[\\w:/.?=&+;\"\\s]*", "").replace("&#160;", " ").replace(">", "");
+					strGuildMembership = mGuildMembership.group(0).split("Guild&#160;Membership:</td><td>")[1]
+							.replace("</a></td", "").replaceAll("<a[\\s]*href[\\s]*=[\\s]*\"[\\w:/.?=&+;\"\\s]*", "")
+							.replace("&#160;", " ").replace(">", "");
 				}
-				
+
 				// FORMER NAMES
 				String patternFormerNames = "<td>Former Names:</td><td>[\\s\\w,]*";
 				Pattern rFormerNames = Pattern.compile(patternFormerNames);
@@ -149,7 +159,7 @@ public class GetInfos {
 				while (mFormerNames.find()) {
 					strFormerNames = mFormerNames.group(0).split("><td>")[1].trim().replace(",", " and ");
 				}
-				
+
 				// FORMER WORLD
 				String patternFormerWorld = "<td>Former World:</td><td>[\\s\\w]*";
 				Pattern rFormerWorld = Pattern.compile(patternFormerWorld);
@@ -158,7 +168,7 @@ public class GetInfos {
 				while (mFormerWorld.find()) {
 					strFormerWorld = mFormerWorld.group(0).split("><td>")[1].trim();
 				}
-				
+
 				// MARRIED TO
 				String patternMarriedTo = "<td>Married To:</td><td>[\\w\\s<=\":/.?&>+]*[&#160;]*[\\w]*";
 				Pattern rMarriedTo = Pattern.compile(patternMarriedTo);
@@ -168,44 +178,146 @@ public class GetInfos {
 					System.out.println(mMarriedTo.group(0));
 					strMarriedTo = mMarriedTo.group(0).split("\">")[1].split("</a>")[0].replace("&#160;", " ").trim();
 				}
-				
-				
-				
 
-				String strMensagem = strName +
-						": Level: " + strLevel +
-						", Voc: " + strVocation +
-						", World: " + strWorld +
-						", Sex: " + strSex +
-						", Residence: " + strResidence + 
-						", AP: " + strAchievementPoints + 
-						", Last Login: " + strLastLogin + 
-						", Acc Status: " + strAccountStatus;
-				
-				if(!strHouse.equals("")){
-					strMensagem += ", House: " + strHouse;
+				Map<String, String> mapInformations = new HashMap<String, String>();
+
+				mapInformations.put("name", strName);
+				mapInformations.put("level", strLevel);
+				mapInformations.put("vocation", strVocation);
+				mapInformations.put("world", strWorld);
+				mapInformations.put("sex", strSex);
+				mapInformations.put("residence", strResidence);
+				mapInformations.put("achievement", strAchievementPoints);
+				DinstinctFunctions df = new DinstinctFunctions();
+				mapInformations.put("last_login", df.CharDTtoMySQLDT(strLastLogin));
+				df = null;
+				mapInformations.put("account_status", strAccountStatus);
+
+				if (strHouse.equals("")) {
+					mapInformations.put("house", null);
+				} else {
+					mapInformations.put("house", strHouse);
 				}
-				
-				if(!strGuildMembership.equals("")){
-					strMensagem += ", Guild: " + strGuildMembership;
+
+				if (strGuildMembership.equals("")) {
+					mapInformations.put("guild", null);
+				} else {
+					mapInformations.put("guild", strGuildMembership);
 				}
-				
-				if(!strFormerNames.equals("")){
-					strMensagem += ", Former Names: " + strFormerNames;
+
+				if (strFormerNames.equals("")) {
+					mapInformations.put("former_names", null);
+				} else {
+					mapInformations.put("former_names", strFormerNames);
 				}
-				
-				if(!strFormerWorld.equals("")){
-					strMensagem += ", Former World: " + strFormerWorld;
+
+				if (strFormerWorld.equals("")) {
+					mapInformations.put("former_worlds", null);
+				} else {
+					mapInformations.put("former_worlds", strFormerWorld);
 				}
-				
-				if(!strMarriedTo.equals("")){
-					strMensagem += ", Married to: " + strMarriedTo;
+
+				if (strMarriedTo.equals("")) {
+					mapInformations.put("married_to", null);
+				} else {
+					mapInformations.put("married_to", strMarriedTo);
 				}
-				
-				Principal.enviarMensagem(canal, remetente, strMensagem);
+
+				return mapInformations;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return null;
+	}
+
+	public ArrayList<String> getWorldsName() {
+		try {
+			URL url = new URL("https://secure.tibia.com/community/?subtopic=worlds");
+			int responseCode = 0;
+			String content = "";
+			while (responseCode != 200) {
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestProperty("User-Agent",
+						"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+				conn.setConnectTimeout(1000);
+				responseCode = conn.getResponseCode();
+
+				if (responseCode == 200) {
+					// open the stream and put it into BufferedReader
+					BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "ISO-8859-1"));
+
+					String inputLine;
+
+					while ((inputLine = br.readLine()) != null) {
+						content += inputLine;
+					}
+					br.close();
+				}
+
+				conn.disconnect();
+				conn = null;
+			}
+			
+			ArrayList<String> worlds = new ArrayList<String>();
+
+			String pattern = "world=[\\w]*";
+			Pattern r = Pattern.compile(pattern);
+			Matcher m = r.matcher(content);
+
+			while (m.find()) {
+				worlds.add(m.group(0).replace("world=", ""));
+			}
+
+			return worlds;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public ArrayList<String> getPlayersByWorld(String strWorldName) {
+		try {
+			URL url = new URL("https://secure.tibia.com/community/?subtopic=worlds&world="+strWorldName);
+			int responseCode = 0;
+			String content = "";
+			while (responseCode != 200) {
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestProperty("User-Agent",
+						"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+				conn.setConnectTimeout(99999);
+				responseCode = conn.getResponseCode();
+
+				if (responseCode == 200) {
+					// open the stream and put it into BufferedReader
+					BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "ISO-8859-1"));
+
+					String inputLine;
+
+					while ((inputLine = br.readLine()) != null) {
+						content += inputLine;
+					}
+					br.close();
+				}
+
+				conn.disconnect();
+				conn = null;
+			}
+			
+			ArrayList<String> players = new ArrayList<String>();
+
+			String pattern = "&name=[\\w\\s]*";
+			Pattern r = Pattern.compile(pattern);
+			Matcher m = r.matcher(content);
+
+			while (m.find()) {
+				players.add(m.group(0).replace("&name=", ""));
+			}
+
+			return players;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
--- TibiaTracker/src/core/Main.java
+++ TibiaTracker/src/core/Main.java
@@ -1,10 +1,40 @@
 package core;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		GetInfos gi = new GetInfos();
+		
+		ArrayList<String> worlds = new ArrayList<String>();
+		ArrayList<String> players = new ArrayList<String>();
+		
+		worlds = gi.getWorldsName();
+		
+		for(String w : worlds) {
+			System.out.println("Getting players for: "+w);
+			ArrayList<String> playersWorld = new ArrayList<String>();
+			playersWorld = gi.getPlayersByWorld(w);
+			System.out.println(playersWorld.size() +" players found");
+			players.addAll(playersWorld);
+		}
+		
+		System.out.println(players.size());
+		
+		/*
+		ArrayList<String> CharNames = new ArrayList<String>();
+		
+		CharNames.add("Kharsek");
+		CharNames.add("Lyh");
+		CharNames.add("Kyoro");
+		CharNames.add("Kyorinho");
+		CharNames.add("COnefi");
+		for(String c : CharNames) {
+			new ControllerBuscaInfoChar(c).start();
+		}
+		*/
+		
 	}
 
 }
